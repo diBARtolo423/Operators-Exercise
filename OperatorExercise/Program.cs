ff77de75d1699973b9ee4a6c76ed18b4689640ef
ross dibartolo <dibartoloross@gmail.com>
2022-12-28 13:55:25 -0500

    second commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -10,6 +10,15 @@
             var remainder = a % b;
             
             Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+            Console.WriteLine("What is the radius of your circle:");
+            var radius = double.Parse(Console.ReadLine());
+            Console.WriteLine(AreaofCircle(radius));
+        }
+        public static double AreaofCircle(double radius)
+        {
+            var area = Math.PI * Math.Pow(radius, 2);
+            return area; 
+            
         }
     }
 }
